4a3d5b9926931fef503df92683daf3a3325d9ab9
muharrem <marslandevops@gmail.com>
2024-04-25 10:25:35 +0200

    modified file

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-//@RestController
-//public class DemoApplication {
+@RestController
+public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
